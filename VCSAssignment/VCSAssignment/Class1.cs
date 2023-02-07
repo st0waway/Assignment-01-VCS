fe8625b5c6287c01e205616f27972dbb90c30c37
st0waway <lumixbd@gmail.com>
2023-02-07 16:15:20 +0200

    Modify Class1

--- VCSAssignment/VCSAssignment/Class1.cs
+++ VCSAssignment/VCSAssignment/Class1.cs
@@ -7,6 +7,7 @@
 			Console.WriteLine("Added some functionality");
 			Console.WriteLine("Added the functionality of feature number two");
 			Console.WriteLine("Added feature_1 functionality");
+			Console.WriteLine("Modified Class1");
 		}
 	}
 }
