dea99a0911dbe34be41abd15786acd072d9b89e2
st0waway <lumixbd@gmail.com>
2023-02-07 18:24:47 +0200

    Remove functionality from main

--- VCSAssignment/VCSAssignment/Program.cs
+++ VCSAssignment/VCSAssignment/Program.cs
@@ -5,8 +5,6 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
-			Console.WriteLine("Add some other functionality");
-			Console.WriteLine("Added some functionality");
 		}
 	}
 }
