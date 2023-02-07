d7b69cc11ec6e95cb7f57d042a1cc53d92769091
st0waway <lumixbd@gmail.com>
2023-02-07 17:00:31 +0200

    Modify files

--- VCSAssignment/VCSAssignment/Class1.cs
+++ VCSAssignment/VCSAssignment/Class1.cs
@@ -7,8 +7,6 @@
 			Console.WriteLine("Added some functionality");
 			Console.WriteLine("Added the functionality of feature number two");
 			Console.WriteLine("Added feature_1 functionality");
-			Console.WriteLine("Modified Class1");
-			Console.WriteLine("Modified Class1 again");
 		}
 	}
 }
--- VCSAssignment/VCSAssignment/Program.cs
+++ VCSAssignment/VCSAssignment/Program.cs
@@ -5,8 +5,6 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
-			Console.WriteLine("Modified program");
-			Console.WriteLine("Modify program");
 		}
 	}
 }
