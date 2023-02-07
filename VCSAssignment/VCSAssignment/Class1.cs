e316ace2a9ce208ed153e862d28e8065ab0bfca9
st0waway <lumixbd@gmail.com>
2023-02-07 16:51:44 +0200

    Revert "Remove Class1"
    This reverts commit eeccd09c7bc792e5056afcf36eaee0253fd69076.

---
+++ VCSAssignment/VCSAssignment/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace VCSAssignment
+{
+	internal class Class1
+	{
+		public static void Method()
+		{
+			Console.WriteLine("Added some functionality");
+			Console.WriteLine("Added the functionality of feature number two");
+			Console.WriteLine("Added feature_1 functionality");
+			Console.WriteLine("Modified Class1");
+		}
+	}
+}
